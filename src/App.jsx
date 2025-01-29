4f701391d94a87b5cd78708c5ca8064d6543cb0b
Kaushalr21 <kaushalram021@gmail.com>
2025-01-29 22:08:52 +0530

    changed

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import ele from './ele.jpg';
 
 function App() {
   return (
-    <div>
+    <div className="output">
        <center> 
         <img src={ele} style={{ width: "100%", height: "150px", marginTop: "0" }}/> 
         <h3 style={{ color: "white", margin:"0"}}>Products madeup of 100% Coconut Oil</h3>
