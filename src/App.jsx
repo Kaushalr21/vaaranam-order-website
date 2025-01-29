51f0a5ed1ff926d781d7d5cdf2118c30892bc70c
Kaushalr21 <kaushalram021@gmail.com>
2025-01-29 23:07:41 +0530

    perfected

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="output">
        <center> 
-        <img src={ele} style={{ width: "100%", height: "150px", marginTop: "0" }}/> 
+        <img src={ele} style={{ width: "750px", height: "250px", marginTop: "0" }}/> 
         <h3 style={{ color: "white", margin:"0"}}>Products madeup of 100% Coconut Oil</h3>
         </center>
        <OrderForm/>
